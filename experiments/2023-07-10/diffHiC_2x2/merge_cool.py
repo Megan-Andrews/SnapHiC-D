cda0705b5d27439c3aa6eba98541de7f4b34988d
Megan-Andrews <megan.r.andrews@gmail.com>
2023-07-11 16:21:04 -0700

    merge.py edits

--- experiments/2023-07-10/diffHiC_2x2/merge_cool.py
+++ experiments/2023-07-10/diffHiC_2x2/merge_cool.py
@@ -1,6 +1,21 @@
 import cooler
 
 
+file_path = '/home/maa160/SnapHiC-D/experiments/2023-07-10/diffHiC_2x2/file_lists/MG_10kb_Batch_A_file_list.txt'
+directory = '/project/compbio-lab/scHi-C/Lee2019/Human_single_cell_10kb_cool/'
+output_path = '/project/compbio-lab/scHi-C/Lee2019/pseudo-bulk_data/MG/MG_10kb_Batch_A.cool'
+
+filenames = []
+
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        file_name = line.strip()  # Remove leading/trailing whitespace and newline characters
+        filenames.append(directory + file_name)
+
+
+cooler.merge_coolers(output_path, filenames, mergebuf=1000000)
+
 file_path = '/home/maa160/SnapHiC-D/experiments/2023-07-10/diffHiC_2x2/file_lists/MG_10kb_Batch_B_file_list.txt'
 directory = '/project/compbio-lab/scHi-C/Lee2019/Human_single_cell_10kb_cool/'
 output_path = '/project/compbio-lab/scHi-C/Lee2019/pseudo-bulk_data/MG/MG_10kb_Batch_B.cool'
