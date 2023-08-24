cc146c6188649d1243530a8934d16f262d96b1ab
Megan-Andrews <megan.r.andrews@gmail.com>
2023-08-24 11:39:59 -0700

    added visualization script

--- experiments/2023-08-21/test_cool_visualization.py
+++ experiments/2023-08-21/test_cool_visualization.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import cooler
 
+chrom_size_path = "../../ext/hg19.chrom.sizes.ordered"
 
 def visualize_hic(coolfile, matrixfile, outputfile): 
     c = cooler.Cooler(coolfile)
@@ -18,7 +19,26 @@ def visualize_hic(coolfile, matrixfile, outputfile):
     matrix_df.columns = columns
     matrix_df = matrix_df[matrix_df["1st_chr"] == matrix_df["2nd_chr"]] # only include intra-chromosome pairs
     matrix_df = matrix_df[matrix_df["1st_chr"] == "human_chr22"] # only include intra-chromosome pairs
+
+    def get_chrom_offsets(bins_df):
+        chrom_offset = {chrom: bins_df[bins_df['chrom'] == chrom].index[0]
+                        for chrom in bins_df['chrom'].cat.categories}
+        return chrom_offset
+
+    chrom_sizes = pd.read_csv(chrom_size_path, sep='\t',index_col=0, header=None).squeeze(axis=1)
+    bins_df = cooler.binnify(chrom_sizes, 500000)
+    chrom_offsets = get_chrom_offsets(bins_df)
+
+    mask = matrix_df["1st_chr"] == "human_chr22"
+        
+    # Update binId_1 and binId_2 using .loc
+    matrix_df.loc[mask, "binId_1"] -= chrom_offsets["chr22"]
+    matrix_df.loc[mask, "binId_2"] -= chrom_offsets["chr22"]
+
     matrix_df = matrix_df[['binId_1', 'binId_2', 'counts']]
+    matrix_reflection = matrix_df.copy()
+    matrix_reflection.columns = ['binId_2', 'binId_1', 'counts']
+    matrix_df = pd.concat([matrix_df, matrix_reflection], ignore_index=True)
     pivot_df = matrix_df.pivot(index='binId_1', columns='binId_2', values='counts')
     pivot_df = pivot_df.fillna(0)
 
@@ -30,7 +50,7 @@ def visualize_hic(coolfile, matrixfile, outputfile):
 
     ax1 = axes[0]
     im1 = ax1.imshow(cool_matrix, cmap='coolwarm', origin="lower")
-    ax1.set_title("Cool Matrix")
+    ax1.set_title(".cool")
     ax1.set_xlabel("Genomic Position")
     ax1.set_ylabel("Genomic Position")
     cbar1 = fig.colorbar(im1, ax=ax1)
@@ -38,7 +58,7 @@ def visualize_hic(coolfile, matrixfile, outputfile):
 
     ax2 = axes[1]
     im2 = ax2.imshow(pivot_df, cmap='coolwarm', origin="lower")
-    ax2.set_title("Matrix DataFrame")
+    ax2.set_title(".matrix")
     ax2.set_xlabel("Genomic Position")
     ax2.set_ylabel("Genomic Position")
     cbar2 = fig.colorbar(im2, ax=ax2)
