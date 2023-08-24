6bf7a35c667dc2a523df40726956c43b43275197
Megan-Andrews <megan.r.andrews@gmail.com>
2023-08-24 10:27:48 -0700

    fixed bug

--- experiments/2023-08-21/count_library_cell-lines.py
+++ experiments/2023-08-21/count_library_cell-lines.py
@@ -10,6 +10,6 @@ file_df = pd.DataFrame(file_array, columns=["file_name"])
 file_df["cell_line"] = file_df["file_name"].apply(lambda x: x.split("_")[0])
 file_df["library"] = file_df["file_name"].apply(lambda x: x.split("_")[1])
 
-file_df["cell_line"].value_counts()
-file_df["library"].value_counts()
+print(file_df["cell_line"].value_counts())
+print(file_df["library"].value_counts())
 
