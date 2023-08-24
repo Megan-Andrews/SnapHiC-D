b3c6a68a9ba805a79a335a9b05c6ce19d2c2a821
Megan-Andrews <megan.r.andrews@gmail.com>
2023-08-24 13:48:04 -0700

    rwr script

---
+++ experiments/2023-08-24/run_rwr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+############################################################################
+###                            User Variables                            ###
+############################################################################
+
+SnapHiC_D_dir="/home/maa160/SnapHiC-D"
+file_list="/project/compbio-lab/scHi-C/Kim2020/Kim2020_cool/file_list.txt"
+input_dir="/project/compbio-lab/scHi-C/Kim2020/Kim2020_cool"
+output_dir="/project/compbio-lab/scHi-C/Kim2020/Kim2020_cool_rwr"
+res=500000
+chr_lens="/home/maa160/SnapHiC-D/ext/hg19.chrom.sizes"
+format="cooler"
+window_size=10000000
+rp=0.05
+extension=".cool"
+upper_distance=2000000
+
+############################################################################
+
+
+python ${SnapHiC_D_dir}/python/snapHiC_preprocessing_mpi.py -f $file_list --indir $input_dir -o $out_dir -r $bin_size -l $chr_lens --format $format --window-size $window_size --rp $rp --extension $extension --upper-distance $upper_distance
+
