50bc93515b9049506bb4d8163f2d380455b408c6
Megan-Andrews <megan.r.andrews@gmail.com>
2023-08-24 13:58:42 -0700

    write file script

--- experiments/2023-08-24/write_file_list.py
+++ experiments/2023-08-24/write_file_list.py
@@ -10,4 +10,4 @@ file_list = os.listdir(directory_path)
 with open("/project/compbio-lab/scHi-C/Kim2020/Kim2020_cool/file_list.txt", "w") as file:
     # Write each file name to the file_list.txt, one per line
     for filename in file_list:
-        file.write(filename + "\n")
+        file.write(directory_path +filename + "\n")
