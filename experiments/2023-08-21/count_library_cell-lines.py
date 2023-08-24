c94aeda98af5b2a140ebea7b414733c4b34c8ff6
Megan-Andrews <megan.r.andrews@gmail.com>
2023-08-24 10:29:44 -0700

    fixed bug

--- experiments/2023-08-21/count_library_cell-lines.py
+++ experiments/2023-08-21/count_library_cell-lines.py
@@ -7,8 +7,8 @@ for file in os.listdir("/project/compbio-lab/scHi-C/Kim2020/Kim2020_cool"):
     file_array.append(file)
 
 file_df = pd.DataFrame(file_array, columns=["file_name"])
-file_df["cell_line"] = file_df["file_name"].apply(lambda x: x.split("_")[0])
-file_df["library"] = file_df["file_name"].apply(lambda x: x.split("_")[1])
+file_df["cell_line"] = file_df["file_name"].apply(lambda x: x.split("_")[2])
+file_df["library"] = file_df["file_name"].apply(lambda x: x.split("_")[3])
 
 print(file_df["cell_line"].value_counts())
 print(file_df["library"].value_counts())
