ed936628e4355b959102b3b1b3a450babb1d8df9
maa160 <maa160@cs-venus-03.cmpt.sfu.ca>
2023-09-29 01:10:18 -0700

    modified plot

--- experiments/2023-09-18/plot_SCC.py
+++ experiments/2023-09-18/plot_SCC.py
@@ -14,27 +14,30 @@ GM12878_pairs = "GM12878_selected_pairs.txt"
 HFF_pairs =  "HFF_selected_pairs.txt"
 pairs_list = [GM12878_HFF_pairs, GM12878_pairs, HFF_pairs]
 
-plt.figure(figsize=(15, 5))  
+#plt.figure(figsize=(15, 5))  
 
 violins = []
+fig, axes = plt.subplots(1, len(pairs_list), figsize=(15, 5), sharey=True)
+
 for j, p in enumerate(pairs_list):
-    plt.subplot(131+j)
-    plt.xticks([1,2,3,4,5], ["Raw","RWR", "scVI","Higashi K0","Higashi K5"])
+    ax = axes[j]  # Get the current axis
+    ax.set_xticks([1, 2, 3, 4, 5])
+    ax.set_xticklabels(["Raw", "RWR", "scVI", "Higashi K0", "Higashi K5"])
+    
     for i, d in enumerate(dirs):
-        temp_df  = pd.read_csv(os.path.join(d,p), sep='\t')
-        # temp_df.columns = ["cool1", "cool2", "HiCRep_SCC"]
-        #subplot_index = len(dirs) * j + i + 1
-        # Create subplot with four columns
-        #plt.subplot(len(pairs_list), len(dirs), subplot_index)
+        temp_df = pd.read_csv(os.path.join(d, p), sep='\t')
         temp_df["HiCRep_SCC"] = pd.to_numeric(temp_df["HiCRep_SCC"])
-        v = plt.violinplot(temp_df["HiCRep_SCC"], showmeans=True, showmedians=True)
-        violins.append(v['bodies'][0])
-        plt.xlabel('Groups')
-        plt.ylabel('Similarity Scores (SCC)')
-	# plt.legend()
-        plt.title(p.replace(".txt", ""))
-        plt.grid(axis='y')
-    plt.legend(violins,  ["Raw","scVI","Higashi K0","Higashi K5"])
-    plt.tight_layout()
+        
+        v = ax.violinplot(temp_df["HiCRep_SCC"], showmeans=True, showmedians=True)
+        ax.set_xlabel('Groups')
+        ax.set_ylabel('Similarity Scores (SCC)')
+        ax.set_title(p.replace("_selected_pairs.txt", ""))
+        ax.grid(axis='y')
+
+# Add a legend to the first subplot (you can customize this as needed)
+axes[0].legend(violins, ["Raw", "RWR", "scVI", "Higashi K0", "Higashi K5"])
+
+plt.tight_layout()
 plt.savefig(os.path.join("/project/compbio-lab/scHi-C/Kim2020/similarity_scores/SCC_plot_2.png"))
 plt.savefig(os.path.join("/home/maa160/SnapHiC-D/experiments/2023-09-18/SCC_plot_2.png"))
+
